4871196b908857c3917236dab5ec177bc871f9be
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-04-15 19:45:54 +0100

    feat: utils

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+import re
 import sqlite3
+
+from flask import Flask
 
 DB_PATH = "db/coisascomgosto.db"
 
@@ -50,3 +54,44 @@ def build_update_query(
     query = f"UPDATE {table} SET {set_clause} WHERE {where_clause}"
     values = tuple(data.values()) + where_values
     return query, values
+
+
+def format_datetime(dt):
+    """Helper function to format datetime as DD/MM/YYYY HH:MM"""
+    if not dt:
+        return None
+
+    if isinstance(dt, str):
+        try:
+            dt = datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return dt
+
+    return dt.strftime("%d/%m/%Y %H:%M")
+
+
+def format_date(dt):
+    return datetime.strftime(dt, "%d/%m/%Y") if dt else None
+
+
+def is_email_valid(email):
+    """Function that validates an email"""
+    regex = re.compile(
+        r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
+    )
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
+def print_routes(prefix: str, app: Flask):
+    """Prints the routes with a given endpoint prefix (e.g., 'api' or 'web')"""
+    print("-" * 120)
+    print(f"{prefix.upper():^120}")
+    print("-" * 120)
+
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint.startswith(f"{prefix}."):
+            methods = ",".join(sorted(rule.methods - {"HEAD", "OPTIONS"}))
+            print(f"{methods:8} {rule.rule:50} -> {rule.endpoint}")
