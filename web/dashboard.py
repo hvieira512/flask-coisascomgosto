3c7002d450db65dbb85277f45eb0151f9c97c5e3
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-04-19 18:20:17 +0100

    feat: dashboard base html

--- web/dashboard.py
+++ web/dashboard.py
@@ -1,11 +1,32 @@
 from flask import Blueprint, redirect, render_template, session, url_for
 
-bp = Blueprint("dashboard", __name__)
+bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
 
-@bp.route("/dashboard")
+@bp.route("/")
+def index():
+    return redirect(url_for("web.dashboard.home"))
+
+
+@bp.route("/home")
 def home():
     if "username" not in session:
         return redirect(url_for("web.auth.home"))
 
-    return render_template("dashboard/index.html")
+    return render_template("dashboard/home.html")
+
+
+@bp.route("/products")
+def products():
+    if "username" not in session:
+        return redirect(url_for("web.auth.home"))
+
+    return render_template("dashboard/products.html")
+
+
+@bp.route("/stats")
+def stats():
+    if "username" not in session:
+        return redirect(url_for("web.auth.home"))
+
+    return render_template("dashboard/stats.html")
